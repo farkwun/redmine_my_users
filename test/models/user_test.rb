d783c30e0847b8de7669cf98c19a21a25dd433e7
farkwun <bryan.chu@ryerson.ca>
2016-08-02 17:56:58 -0400

    Adding tests for user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,26 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class UserTest < ActiveSupport::TestCase
+  include TestSetupMethods
+  include TestHelperMethods
+  def setup
+    @show_debug = false
+  end
+
+  test "parent_id_required?" do
+    @alice = create_user('alice', 'alice@doe.com')
+    assert @alice.errors.any?,
+      "User should have failed creation - #{@alice.inspect}"
+  end
+
+  test "cannot_set_parent_id_to_child_id" do
+    flunk('Functionality does not exist') # comment this out to test actual code
+    @alice = create_user('alice','alice@doe.com', 1)
+    @bob = create_user('bob','bob@doe.com', @alice.id)
+
+    find_user(@alice).update({:parent_id => @bob.id.to_i})
+
+    refute find_user(@alice).parent_id.to_i == @bob.id.to_i,
+      "Should not allow update with child as parent - #{find_user(@alice).inspect}"
+  end
+end
